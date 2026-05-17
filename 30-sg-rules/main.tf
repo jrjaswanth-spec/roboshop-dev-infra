6debb262d3bf3ab0f2785901c75684b2978d016f
Jaswanth <jrjaswanth@gmail.com>
2026-05-17 19:04:11 +0530

    vxxxs

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "rabbitmq_bastion" {
 resource "aws_security_group_rule" "mysql_bastion" {
   type = "ingress"
   security_group_id = local.mysql_sg_id
-   source_security_group_id = local.mysql_sg_id
+   source_security_group_id = local.bastion_sg_id
   from_port         = 22
   protocol       = "tcp"
   to_port           = 22
