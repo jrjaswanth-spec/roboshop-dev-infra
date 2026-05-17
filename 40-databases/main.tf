5083e6ec45c2412004db8f5ca118e5cdce954330
Jaswanth <jrjaswanth@gmail.com>
2026-05-17 12:41:11 +0530

    adding

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -42,47 +42,47 @@ resource "aws_instance" "mongodb" {
   }
 
 
-resource "aws_instance" "redis" {
-  ami                    = local.ami_id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [local.redis_sg_id]
-  subnet_id              = local.database_subnet_id
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.common_name_suffix}-redis" #roboshop-dev-mongodb
-    }
-  )
+# resource "aws_instance" "redis" {
+#   ami                    = local.ami_id
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [local.redis_sg_id]
+#   subnet_id              = local.database_subnet_id
+#   tags = merge(
+#     local.common_tags,
+#     {
+#       Name = "${local.common_name_suffix}-redis" #roboshop-dev-mongodb
+#     }
+#   )
 
-}
- resource "terraform_data" "redis" {
-  triggers_replace = [
-    aws_instance.redis.id,
-  ]
+# }
+#  resource "terraform_data" "redis" {
+#   triggers_replace = [
+#     aws_instance.redis.id,
+#   ]
 
   
- connection {
-      type = "ssh"
-      user = "ec2-user"
-      password = "DevOps321"
-      host = aws_instance.redis.private_ip
-    }
+#  connection {
+#       type = "ssh"
+#       user = "ec2-user"
+#       password = "DevOps321"
+#       host = aws_instance.redis.private_ip
+#     }
     
-    # terraform copies this file to mongodb server
-  provisioner "file" {
-    source = "bootstrap.sh"
-    destination = "/tmp/bootstrap.sh"
+#     # terraform copies this file to mongodb server
+#   provisioner "file" {
+#     source = "bootstrap.sh"
+#     destination = "/tmp/bootstrap.sh"
     
-  }
+#   }
 
-  provisioner "remote-exec" {
-    inline = [ 
-        "chmod +x /tmp/bootstrap.sh",
-        "sudo sh  /tmp/bootstrap.sh redis"
-     ]
-  }
+#   provisioner "remote-exec" {
+#     inline = [ 
+#         "chmod +x /tmp/bootstrap.sh",
+#         "sudo sh  /tmp/bootstrap.sh redis"
+#      ]
+#   }
     
-  }
+#   }
 
 
 
@@ -90,19 +90,19 @@ resource "aws_instance" "redis" {
 
 
 
-resource "aws_instance" "rabbitmq" {
-  ami                    = local.ami_id
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [local.rabbitmq_sg_id]
-  subnet_id              = local.database_subnet_id
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${local.common_name_suffix}-rabbitmq" #roboshop-dev-mongodb
-    }
-  )
+# resource "aws_instance" "rabbitmq" {
+#   ami                    = local.ami_id
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [local.rabbitmq_sg_id]
+#   subnet_id              = local.database_subnet_id
+#   tags = merge(
+#     local.common_tags,
+#     {
+#       Name = "${local.common_name_suffix}-rabbitmq" #roboshop-dev-mongodb
+#     }
+#   )
 
-}
+# }
  resource "terraform_data" "rabbitmq" {
   triggers_replace = [
     aws_instance.rabbitmq.id,
