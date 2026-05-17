9bc9851a9b4bd0f3590a75062692bef607b6a059
Jaswanth <jrjaswanth@gmail.com>
2026-05-17 12:47:48 +0530

    adding

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -103,34 +103,34 @@ resource "aws_instance" "mongodb" {
 #   )
 
 # }
- resource "terraform_data" "rabbitmq" {
-  triggers_replace = [
-    aws_instance.rabbitmq.id,
-  ]
+#  resource "terraform_data" "rabbitmq" {
+#   triggers_replace = [
+#     aws_instance.rabbitmq.id,
+#   ]
 
   
- connection {
-      type = "ssh"
-      user = "ec2-user"
-      password = "DevOps321"
-      host = aws_instance.rabbitmq.private_ip
-    }
+#  connection {
+#       type = "ssh"
+#       user = "ec2-user"
+#       password = "DevOps321"
+#       host = aws_instance.rabbitmq.private_ip
+#     }
     
     # terraform copies this file to mongodb server
-  provisioner "file" {
-    source = "bootstrap.sh"
-    destination = "/tmp/bootstrap.sh"
+  # provisioner "file" {
+  #   source = "bootstrap.sh"
+  #   destination = "/tmp/bootstrap.sh"
     
-  }
+  # }
 
-  provisioner "remote-exec" {
-    inline = [ 
-        "chmod +x /tmp/bootstrap.sh",
-        "sudo sh  /tmp/bootstrap.sh rabbitmq"
-     ]
-  }
+  # provisioner "remote-exec" {
+  #   inline = [ 
+  #       "chmod +x /tmp/bootstrap.sh",
+  #       "sudo sh  /tmp/bootstrap.sh rabbitmq"
+  #    ]
+  # }
     
-  }
+  # }
 
 
 
