98bbff970c529e62ca766caa232298d0155bff1d
Jaswanth <jrjaswanth@gmail.com>
2026-05-18 12:43:01 +0530

    saw

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "bastion" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id              = local.public_subnet_id
-  user_data = file ("bastion.sh")
+  iam_instance_profile   = aws_iam_instance_profile.bastion.name
+  user_data              = file("bastion.sh")
   tags = merge(
     local.common_tags,
     {
@@ -14,3 +15,7 @@ resource "aws_instance" "bastion" {
 
 
 
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "BastionTerraformAdmin"
+}
