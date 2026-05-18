e271fe322e2af2659f0f5fc8f75d91ee0f30c5d4
Jaswanth <jrjaswanth@gmail.com>
2026-05-18 14:33:16 +0530

    scaw

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
   common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id = data.aws_ami.joindevops.id
   common_tags = {
     project = var.project_name
