70fd26c8052e4b2d892d57148701c3fbccea866e
Jaswanth <jrjaswanth@gmail.com>
2026-05-20 16:53:07 +0530

    loi

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -23,7 +23,7 @@ resource "aws_lb" "backend_alb" {
 
 
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "backend_alb" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
