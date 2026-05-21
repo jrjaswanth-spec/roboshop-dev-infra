4824fe35a4caf0f53dddb2a4cd35471f37be6da6
Jaswanth <jrjaswanth@gmail.com>
2026-05-21 12:29:56 +0530

    pre

---
+++ 70-acm/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
+
+  common_tags = {
+    project = var.project_name
+    environment = var.environment
+    Terraform = "true"
+  }
+}
---
+++ 70-acm/variables.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "jrdaws.life"
+  
+}
+
+variable "zone_id" {
+    default = "Z00835102KF6ZT3AHQVW7"
+    type = string
+  
+}
