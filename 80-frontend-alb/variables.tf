914bd64ccba80e715500ef893172f67b08b7b942
Jaswanth <jrjaswanth@gmail.com>
2026-05-21 17:04:54 +0530

    ddssa

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,19 @@
+variable "project_name"{
+    default = "roboshop"
+  
+}
+
+variable "environment" {
+    default = "dev"  
+}
+
+variable "zone_id" {
+    default = "Z00835102KF6ZT3AHQVW7"
+    type = string
+  
+}
+
+variable "domain_name" {
+    default = "jrdaws.life"
+  
+}
