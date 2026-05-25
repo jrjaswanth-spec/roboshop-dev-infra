823079048dbc3b3b78e2732746202d7a9a8a00af
Jaswanth <jrjaswanth@gmail.com>
2026-05-25 19:02:13 +0530

    ffcat

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -228,3 +228,48 @@ resource "aws_security_group_rule" "frontend_frontend_alb" {
   protocol  = "tcp"
   to_port   = 80
 }
+
+resource "aws_security_group_rule" "user_bastion" {
+  type = "ingress"
+  security_group_id = local.user_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port = 22
+  protocol  = "tcp"
+  to_port   = 22
+}
+
+resource "aws_security_group_rule" "cart_bastion" {
+  type = "ingress"
+  security_group_id = local.cart.user_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port = 22
+  protocol  = "tcp"
+  to_port   = 22
+}
+
+resource "aws_security_group_rule" "shipping_bastion" {
+  type = "ingress"
+  security_group_id = local.shipping.user_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port = 22
+  protocol  = "tcp"
+  to_port   = 22
+}
+
+resource "aws_security_group_rule" "payment_bastion" {
+  type = "ingress"
+  security_group_id = local.payment.user_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port = 22
+  protocol  = "tcp"
+  to_port   = 22
+}
+
+resource "aws_security_group_rule" "frontend_bastion" {
+  type = "ingress"
+  security_group_id = local.frontend.user_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port = 22
+  protocol  = "tcp"
+  to_port   = 22
+}
