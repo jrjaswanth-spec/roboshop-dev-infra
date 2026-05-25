0690fb29a6cb1dee98b2a59dd988e52042f14e36
Jaswanth <jrjaswanth@gmail.com>
2026-05-25 19:07:23 +0530

    ffcat

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,12 @@
-module "components" {
-    source = "../../terraform-roboshop-component"
-    component = var.component
-    rule_priority = var.rule_priority
-  }
+# module "component" {
+#     source = "../../terraform-roboshop-component"
+#     component = var.component
+#     rule_priority = var.rule_priority
+#   }
+
+  module "component" {
+    for_each = var.components
+    source = "git::https://github.com/jrjaswanth-spec/roboshop-dev-infra.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
+}
