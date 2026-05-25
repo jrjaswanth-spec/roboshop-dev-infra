ee80816bb7e01c4c7e26ef4421f7f0e85a7dce77
Jaswanth <jrjaswanth@gmail.com>
2026-05-25 17:29:23 +0530

    cat

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -19,3 +19,12 @@ xfs_growfs /home
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum install -y terraform
+
+
+# creating databases
+
+cd /home/ec2-user
+git clone https://github.com/jrjaswanth-spec/roboshop-dev-infra.git
+cd roboshop-dev-infra/40-databases
+terraform init
+terraform apply -auto-approve
