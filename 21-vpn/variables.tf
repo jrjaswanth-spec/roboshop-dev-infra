baa583ec2f6ae0eb309edca5e4ee7183de04c57e
Jaswanth <jrjaswanth@gmail.com>
2026-06-04 21:56:04 +0530

    vpn

---
+++ 21-vpn/variables.tf
@@ -0,0 +1,17 @@
+
+variable "project_name" {
+  default = "roboshop"
+
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00835102KF6ZT3AHQVW7"
+}
+
+variable "domain_name" {
+    default = "jrdaws.life"
+}
--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -315,6 +315,16 @@ resource "aws_security_group_rule" "open_vpn_1194" {
   to_port           = 1194
 }
 
+resource "aws_security_group_rule" "components_vpn" {
+  for_each = local.vpn_ingress_rules
+  type              = "ingress"
+  security_group_id = each.value.sg_id
+  source_security_group_id = local.open_vpn_sg_id
+  from_port         = each.value.port
+  protocol          = "tcp"
+  to_port           = each.value.port
+}
+
 
 # components in backend cant talk with each other, they have to connect first with backend ALB first
 
