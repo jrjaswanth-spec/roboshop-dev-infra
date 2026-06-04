06f2b2d925052db48aa4c8f78567fa7c1bc0ab8a
Jaswanth <jrjaswanth@gmail.com>
2026-06-04 19:47:28 +0530

    Fix bastion userdata

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -13,7 +13,7 @@ sudo yum -y install terraform
 
 # creating databases
 cd /home/ec2-user
-git git clone https://github.com/jrjaswanth-spec/roboshop-dev-infra.git
+git clone https://github.com/jrjaswanth-spec/roboshop-dev-infra.git
 chown ec2-user:ec2-user -R roboshop-dev-infra
 cd roboshop-dev-infra/40-databases
 terraform init
