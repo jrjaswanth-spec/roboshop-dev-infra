f08f5239ee4a5bf10265f40d25694ff4b8d819b9
Jaswanth <jrjaswanth@gmail.com>
2026-06-05 16:25:42 +0530

    lopdd

--- 21-vpn/main.tf
+++ 21-vpn/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "openvpn" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [local.openvpn_sg_id]
   subnet_id              = local.public_subnet_id
-  iam_instance_profile   = aws_iam_instance_profile.bastion.name
   user_data              = file("vpn.sh")
   tags = merge(
     local.common_tags,
