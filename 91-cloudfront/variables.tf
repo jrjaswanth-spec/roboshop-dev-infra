2d04beb717706ba06859923f6333f0edfca60863
Jaswanth <jrjaswanth@gmail.com>
2026-06-06 20:06:58 +0530

    Fdsaawe

---
+++ 91-cloudfront/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z00835102KF6ZT3AHQVW7"
+}
+
+variable "domain_name" {
+    default = "jrdaws.life"
+}
